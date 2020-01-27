055ef998bae7d6b7855e0f32e13dcf273b8ef534
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-01-27 10:07:04 +0100

    Omniauth config: if rails_master_key present

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,9 +260,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  client_id = Rails.application.credentials[:google_omniauth][:client_id]
-  client_secret = Rails.application.credentials[:google_omniauth][:client_secret]
-  config.omniauth :google_oauth2, client_id, client_secret,  {}
+  if ENV['RAILS_MASTER_KEY']
+    client_id = Rails.application.credentials[:google_omniauth][:client_id]
+    client_secret = Rails.application.credentials[:google_omniauth][:client_secret]
+    config.omniauth :google_oauth2, client_id, client_secret,  {}
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

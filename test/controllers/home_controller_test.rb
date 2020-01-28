fa874f0ef502a62e3ab6647433e3a19dc3e67924
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-01-28 15:12:19 +0100

    Changed home controller test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,11 +4,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   test 'should get homepage' do
-    get root_url
-    assert_response :redirect
-    follow_redirect!
     sign_in users(:schmidt)
-    assert_response :success
     get root_url
     assert_response :success
   end

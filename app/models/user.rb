862f7e55222445a9cf05f42616a4f24276fd394c
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-01-28 16:29:02 +0100

    Refactored user model and added model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.find_by(email: data['email'])
+  def self.create_if_not_exist(email)
+    user = User.find_by(email: email)
     return user if user.present?
 
-    User.create(email: data['email'], password: Devise.friendly_token[0, 20])
+    User.create(email: email, password: Devise.friendly_token[0, 20])
   end
 end

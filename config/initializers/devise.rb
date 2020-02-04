facdf51ff04bfbe13bb79b855d204b5605c3ef38
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-02-04 14:27:08 +0100

    Fixed omniauth vulnerability

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  OmniAuth.config.allowed_request_methods = [:post, :get]
+  
   if ENV['RAILS_MASTER_KEY'] || Rails.application.credentials.secret_key_base
     client_id = Rails.application.credentials[:google_omniauth][:client_id]
     client_secret = Rails.application.credentials[:google_omniauth][:client_secret]

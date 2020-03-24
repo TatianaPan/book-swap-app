664f945e155c79dca6e2e40d25d09be254172f8a
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-03-24 15:45:27 +0100

    Added strip whitespaces method on user input in books controller and test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
+  before_validation :strip_input_fields
+
   validates :isbn10, length: { is: 10 },
                      numericality: { only_integer: true, greater_than: 0 },
                      allow_blank: true
@@ -7,4 +9,11 @@ class Book < ApplicationRecord
                      numericality: { only_integer: true, greater_than: 0 },
                      allow_blank: true
   validates :title, :author, :status, presence: true
+
+  private
+
+  def strip_input_fields
+    self.isbn10 = isbn10.strip unless isbn10.nil?
+    self.isbn13 = isbn13.strip unless isbn13.nil?
+  end
 end

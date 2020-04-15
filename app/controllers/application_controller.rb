e4e1736b131938c9ea3adb252dafad22f69b4944
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-04-15 17:28:28 +0200

    Split sign in/sign up views in partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
-  end
 
   private
 

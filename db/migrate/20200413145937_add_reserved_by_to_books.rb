b239d6595dbc091c991b9b228a171e6b4471f82a
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-04-22 15:05:30 +0200

    Add nullable option on borrower_id column in Books

--- db/migrate/20200413145937_add_reserved_by_to_books.rb
+++ db/migrate/20200413145937_add_reserved_by_to_books.rb
@@ -1,7 +1,7 @@
 class AddReservedByToBooks < ActiveRecord::Migration[6.0]
   def change
     change_table :books do |t|
-      t.references :borrower, foreign_key: { to_table: :users }, default: nil
+      t.references :borrower, foreign_key: { to_table: :users }, null: true
     end
   end
 end

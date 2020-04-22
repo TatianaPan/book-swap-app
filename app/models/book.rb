22688c168e2dd45243c45c32429d7e2249f41929
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-04-22 15:05:30 +0200

    Add nullable option on borrower_id column in Books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
-  belongs_to :borrower, class_name: 'User', inverse_of: :borrowed_books
+  belongs_to :borrower, class_name: 'User', inverse_of: :borrowed_books, optional: true
   before_validation :strip_input_fields
 
   validates :isbn10, length: { is: 10 },

08ede49a2a406adc6653c8e25d2704a5ec97512f
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-05-18 16:30:38 +0200

    Add status icons for books in views

--- app/helpers/book_helper.rb
+++ app/helpers/book_helper.rb
@@ -6,4 +6,14 @@ module BookHelper
   def next_reservation_status(book)
     book.status == 'available' ? 'reserved' : 'available'
   end
+
+  def status_icon(book)
+    if book.available?
+      image_pack_tag('media/images/icons8-green-circle-50.png', style: "width: 20px;")
+    elsif book.reserved?
+      image_pack_tag('media/images/icons8-orange-circle-50.png', style: "width: 20px;")
+    elsif book.borrowed?
+      image_pack_tag('media/images/icons8-red-circle-50.png', style: "width: 20px;")
+    end
+  end
 end

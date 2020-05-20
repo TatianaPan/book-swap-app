cb9dae29111a54f8c79ed340df691c3943929fb2
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-05-20 11:24:29 +0200

    Refactor status icon method

--- app/helpers/book_helper.rb
+++ app/helpers/book_helper.rb
@@ -1,4 +1,8 @@
 module BookHelper
+  AVAILABLE_ICON_PATH = 'media/images/icons8-green-circle-50.png'.freeze
+  RESERVED_ICON_PATH = 'media/images/icons8-orange-circle-50.png'.freeze
+  BORROWED_ICON_PATH = 'media/images/icons8-red-circle-50.png'.freeze
+
   def reservation_button_label(book)
     book.status == 'reserved' ? 'Unreserve' : 'Reserve'
   end
@@ -8,12 +12,15 @@ module BookHelper
   end
 
   def status_icon(book)
-    if book.available?
-      image_pack_tag('media/images/icons8-green-circle-50.png', style: 'width: 20px;')
-    elsif book.reserved?
-      image_pack_tag('media/images/icons8-orange-circle-50.png', style: 'width: 20px;')
-    elsif book.borrowed?
-      image_pack_tag('media/images/icons8-red-circle-50.png', style: 'width: 20px;')
-    end
+    icon_path = if book.available?
+                  AVAILABLE_ICON_PATH
+                elsif book.reserved?
+                  RESERVED_ICON_PATH
+                elsif book.borrowed?
+                  BORROWED_ICON_PATH
+                end
+    return if icon_path.nil?
+
+    image_pack_tag(icon_path, style: 'width: 15px;')
   end
 end

120e4b38a4bde05307bbed0c243b94a448a2e504
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-05-25 14:49:28 +0200

    Revert migration for collation set up

--- db/migrate/20200525120229_set_russian_collation_on_books.rb
+++
@@ -1,6 +0,0 @@
-class SetRussianCollationOnBooks < ActiveRecord::Migration[6.0]
-  def up
-    execute 'ALTER TABLE books ALTER COLUMN author TYPE varchar COLLATE "ru_RU.UTF-8";'
-    execute 'ALTER TABLE books ALTER COLUMN title TYPE varchar COLLATE "ru_RU.UTF-8";'
-  end
-end

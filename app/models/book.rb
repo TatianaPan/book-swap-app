0adc38c90d5e4ba929a5257d607a4c6eda649527
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-06-08 16:26:50 +0200

    Add Strippable concern and test

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,13 +4,13 @@ class Book < ApplicationRecord
   pg_search_scope :search_by_author_title, against: %i[first_name last_name title]
 
   STATUSES_REQUIRING_A_BORROWER = %w[reserved borrowed].freeze
-  STRIPPABLE_ATTRIBUTES = %i[first_name last_name title isbn10 isbn13]
+  STRIPPABLE_ATTRIBUTES = %w[first_name last_name title isbn10 isbn13]
 
   belongs_to :user
   belongs_to :borrower, class_name: 'User', inverse_of: :books_on_loan, optional: true
   enum status: { available: 'available', reserved: 'reserved', borrowed: 'borrowed' }
 
-  before_validation :strip_book_input_fields
+  before_validation :strip_input_fields
   before_save :handle_status_and_borrower_correlation
 
   validates :isbn10, length: { is: 10 },
@@ -26,16 +26,6 @@ class Book < ApplicationRecord
   end
 
   private
-
-  def strip_book_input_fields
-    # strip_input_fields(STRIPPABLE_ATTRIBUTES)
-
-    self.first_name = first_name.strip unless first_name.nil?
-    self.last_name = last_name.strip unless last_name.nil?
-    self.title = title.strip unless title.nil?
-    self.isbn10 = isbn10.strip unless isbn10.nil?
-    self.isbn13 = isbn13.strip unless isbn13.nil?
-  end
 
   def handle_status_and_borrower_correlation
     # If the borrower is already set, do not do anything.
---
+++ test/models/concerns/strippable_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class StrippableTest < ActiveSupport::TestCase
+  test 'should strip trailing whitespaces on input fields' do
+    class ExampleClass
+      include Strippable
+      attr_accessor :username, :email, :phone
+      STRIPPABLE_ATTRIBUTES = %w[username email phone]
+
+      def initizlize(username, email, phone)
+        @username = username
+        @email = email
+        @phone = phone
+      end
+    end
+
+    example = ExampleClass.new
+    example.username = ' Donuld Duck'
+    example.email = 'donald@yahoo.com '
+    example.phone = '   '
+    example.strip_input_fields
+
+    assert_equal 'Donuld Duck', example.username
+    assert_equal 'donald@yahoo.com', example.email
+    assert_equal '', example.phone
+  end
+
+end

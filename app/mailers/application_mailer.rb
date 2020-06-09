c46da898846e461cb49916b7e33e4cec778c0107
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-06-09 13:41:22 +0200

    Create reservation mailer, email templates and preview

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'BookSwapApp <bookswapapp.help@gmail.com>'
   layout 'mailer'
 end
---
+++ app/mailers/reservation_mailer.rb
@@ -0,0 +1,12 @@
+class ReservationMailer < ApplicationMailer
+  def reservation(book)
+    @book = book
+    @borrower = book.borrower
+    @owner = book.user
+    @email = book.borrower.email
+
+    mail(to: "#{@owner.first_name} #{@owner.last_name} <#{@owner.email}>",
+         subject: "Your book has been reserved by #{@borrower.first_name} #{@borrower.last_name}",
+         template_name: 'reservation')
+  end
+end

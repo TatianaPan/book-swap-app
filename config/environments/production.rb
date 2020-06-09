a9bbe7e16cb1fae6b37cde255e79ac91b4db1e87
Tatiana Panferova <tatiana.panferova@gmail.com>
2020-06-09 16:45:44 +0200

    Add configuration for gmail SMTP server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  if ENV['RAILS_MASTER_KEY']
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: ENV['DOMAIN'],
+      user_name: Rails.application.credentials.smtp[:username],
+      password: Rails.application.credentials.smtp[:password],
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+  end
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
